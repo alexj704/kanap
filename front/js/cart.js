e6cb029e9d10e19866c55efe86f19034373d7415
Kroumy <alexandre.joignant@gmail.com>
2022-12-08 00:40:56 +0100

    Ajout des produits sur la page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,23 +9,32 @@ if (inLocalStorage == null) {
 
 }
 else {
-    for (let i = 0; i < inLocalStorage.length; i++ ) {
-        const id = inLocalStorage[i].productId
-        const color = inLocalStorage[i].color
-        const divImg = document.createElement('div')
-        const img = document.createElement('img')
-        fetch(`http://localhost:3000/api/products/${id}`)
+    for (let product of inLocalStorage) {
+        fetch(`http://localhost:3000/api/products/${product.productId}`)
             .then(response => response.json())
             .then(data => {
-                const article = document.getElementById('cart__items').appendChild(document.createElement('article'))
-                article.className = 'cart__item'
-                article.setAttribute("data-id",`${id}`)
-                article.setAttribute("data-color", `${color}`)
-                article.appendChild(divImg)
-                divImg.className = 'cart__item__img'
-                divImg.appendChild(img)
-                img.setAttribute('src', `${data.imageUrl}`)
-                img.setAttribute('alt', `${data.altTxt}`)
+                document.getElementById('cart__items').innerHTML +=
+            `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+                <div class="cart__item__img">
+                  <img src="${data.imageUrl}" alt="${data.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${data.name}</h2>
+                    <p>${product.color}</p>
+                    <p>${data.price} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`
             })
             .catch(error => { console.error(error) })
 
